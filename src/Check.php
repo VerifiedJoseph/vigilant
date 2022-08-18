1e0ee662b3c2ab8f265f2b2b6bba6add4e7d5c57
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-08-18 19:06:40 +0100

    Create Check.php

---
+++ src/Check.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Vigilant;
+
+use Vigilant\Config;
+use Vigilant\Notification\Gotify as Gotify;
+
+use SimplePie\SimplePie;
+
+use Exception;
+
+final class Check
+{
+	/**
+	 * @var array $details Feed deatils (name, url, interval and hash)
+	 */
+	private array $details = [];
+
+	/**
+	 * @var Cache $cache Cache class object
+	 */
+	private Cache $cache;
+
+	/**
+	 * @var bool $firstCheck Feed first check status
+	 */
+	private bool $firstCheck = true;
+
+	/**
+	 * Constructor
+	 */
+	function __construct(array $details)
+	{
+		$this->details = $details;
+		$this->cache = new Cache($this->details['hash']);
+	}
+
+	/**
+	 * Run check
+	 */
+	public function run(): void
+	{
+		if ($this->cache->getFirstCheck() !== 0) {
+			$this->firstCheck = false;
+		}
+
+		if (time() >= $this->cache->getNextCheck()) {
+			Output::text('Checking...' . $this->details['name'] . ' (' . $this->details['url'] . ')');
+
+			$feed = new SimplePie();
+			$feed->set_feed_url($this->details['url']);
+			$feed->set_cache_duration(60);
+			$feed->set_cache_location(Config::getSimplePieCachePath());
+			$feed->init();
+			$feed->handle_content_type();
+
+			if ($feed->error()) {
+				throw new Exception($feed->error());
+			}
+
+			$itemHashes = [];
+
+			foreach($feed->get_items() as $item) {
+				$hash = sha1($item->get_permalink());
+				$itemHashes[] = $hash;
+
+				if (in_array($hash, $this->cache->getItems()) === false) {
+					Output::text('Found...' . html_entity_decode($item->get_title()) . ' (' . $hash . ')');
+
+					if ($this->firstRun === false) {
+						$config = [
+							'title' => html_entity_decode($item->get_title()),
+							'message' => strip_tags(html_entity_decode($item->get_description())),
+							'url' => $item->get_permalink()
+						];
+
+						if (Config::get('NOTIFICATION_SERVICE') === 'nfty') {
+							$config['priority'] = Config::get('NOTIFICATION_NFTY_PRIORITY');
+							$config['topic'] = Config::get('NOTIFICATION_NFTY_TOPIC');
+						}
+
+						if (Config::get('NOTIFICATION_SERVICE') === 'gotify') {
+							$config['priority'] = Config::get('NOTIFICATION_GOTIFY_PRIORITY');
+						}
+
+						$this->notify($config);
+					}
+				}
+			}
+
+			if ($this->firstCheck === true) {
+				$this->cache->setFirstCheck();
+				$this->cache->setFeedUrl($this->details['url']);
+			}
+
+			$this->cache->updateItems($itemHashes);
+			$this->cache->updateNextCheck($this->details['interval']);
+			$this->cache->save();
+		}
+	}
+
+	/**
+	 * Send a notification
+	 * 
+	 * @param array $config Notification config
+	 */
+	private function notify(array $config): void
+	{
+		switch (Config::get('NOTIFICATION_SERVICE')) {
+			case 'nfty':
+				//$notification = new Nfty();
+				break;
+			default:
+				$notification = new Gotify();
+		}
+
+		$notification->config($config);
+		$notification->send();
+	}
+}
