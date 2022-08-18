4c57c21e49bfd31ab3634270e96ae9658f1e9d2b
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-08-18 19:27:46 +0100

    Create daemon.php

---
+++ daemon.php
@@ -0,0 +1,31 @@
+<?php
+
+use Vigilant\Config;
+use Vigilant\Feeds;
+use Vigilant\Check;
+use Vigilant\Output;
+
+use Vigilant\Exception\ConfigException;
+
+require('vendor/autoload.php');
+
+try {
+	Config::checkInstall();
+	Config::checkConfig();
+	
+	$feeds = new Feeds();
+	
+	Output::text('---');
+
+	foreach ($feeds->get() as $feed) {
+		try {
+			$check = new Check($feed);
+			$check->run();
+	
+		} catch (Exception $err) {
+			Output::text($err->getMessage());
+		}
+	}
+} catch (ConfigException $err) {
+	Output::text($err->getMessage());
+}
