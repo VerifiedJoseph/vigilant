51961c36a18e098ef46ea836fef0687d56fec235
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-08-18 18:51:38 +0100

    Create ConfigException.php

---
+++ src/Exception/ConfigException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Vigilant\Exception;
+
+use Throwable;
+
+class ConfigException extends \Exception {
+	public function __construct($message, $code = 0, Throwable $previous = null) {
+		$message = 'Config Error: ' . $message;
+
+		parent::__construct($message, $code, $previous);
+	}
+}
