c8f5099786738f554f38003b44ba7ada078e8d80
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-08-18 19:19:50 +0100

    Create Cache.php

---
+++ src/Cache.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace Vigilant;
+
+use Vigilant\Config;
+use Vigilant\Helper\File;
+use Vigilant\Helper\Json;
+
+final class Cache
+{
+	/**
+	 * @var string $filename Cache filename
+	 */
+	private string $filename = '';
+
+	/**
+	 * @var array $data Data from cache file
+	 */
+	private array $data = [
+		'feed_url' => NULL,
+		'first_check' => 0,
+		'next_check' => 0,
+		'items' => []
+	];
+
+	/**
+	 * Constructor
+	 * 
+	 * @var string $hash
+	 */
+	public function __construct(string $hash)
+	{
+		$this->filename = $hash;
+		$this->load();
+	}
+
+	/**
+	 * Get feed URL from cache data 
+	 * 
+	 * @return string|null
+	 */
+	public function getFeedUrl(): string|null
+	{
+		return $this->data['feed_url'];
+	}
+
+	/**
+	 * Get first check unix timestamp
+	 * 
+	 * @return int
+	 */
+	public function getFirstCheck(): int
+	{
+		return $this->data['first_check'];
+	}
+
+	/**
+	 * Get nexct check unix timestamp
+	 * 
+	 * @return int
+	 */
+	public function getNextCheck(): int
+	{
+		if ($this->data['next_check'] === 0) {
+			return time();
+		}
+
+		return $this->data['next_check'];
+	}
+
+	/**
+	 * Get item hashes
+	 * 
+	 * @return array
+	 */
+	public function getItems(): array
+	{
+		return $this->data['items'];
+	}
+
+	/**
+	 * Set first check unix timestamp
+	 */
+	public function setFirstCheck(): void
+	{
+		if ($this->data['first_check'] === 0) {
+			$this->data['first_check'] = time();
+		}
+	}
+
+	/**
+	 * Set feed URL
+	 * 
+	 * @param string $url Feed URL
+	 */
+	public function setFeedUrl(string $url): void
+	{
+		$this->data['feed_url'] = $url;
+	}
+
+	/**
+	 * Update next check using interval value
+	 * 
+	 * @param string $interval Interval in seconds
+	 */
+	public function updateNextCheck(int $interval): void
+	{
+		$this->data['next_check'] = time() + $interval;
+	}
+
+	/**
+	 * Update item hashes
+	 * 
+	 * @param array $items item hashes
+	 */
+	public function updateItems(array $items): void
+	{
+		$this->data['items'] = $items;
+	}
+
+	/**
+	 * Load and decode cache file
+	 */
+	private function load(): void
+	{
+		if (File::exists($this->getPath()) === true) {
+			$json = File::read($this->getPath());
+			$this->data = Json::decode($json);
+		}
+	}
+
+	/**
+	 * Encode and save data to cache file
+	 */
+	public function save(): void
+	{
+		$json = Json::encode($this->data);
+		$json = File::write($this->getPath(), $json);
+	}
+
+	/**
+	 * Get cache file path
+	 * 
+	 * @return string 
+	 */
+	private function getPath(): string
+	{
+		return Config::getCachePath() . DIRECTORY_SEPARATOR . $this->filename;
+	}
+}
