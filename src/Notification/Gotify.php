c62f5a9e624d2d78ec704a2f3b60e72314f57d7a
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-08-18 19:27:28 +0100

    Create Gotify.php

---
+++ src/Notification/Gotify.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Vigilant\Notification;
+
+use Vigilant\Config;
+
+use Gotify\Server;
+use Gotify\Auth\Token;
+use Gotify\Endpoint\Message;
+use Gotify\Exception\GotifyException;
+use Gotify\Exception\EndpointException;
+
+use Exception;
+
+final class Gotify extends Base
+{
+	/**
+	 * Send notification using `verifiedjoseph/gotify-api-php`
+	 */
+	public function send(): void
+	{
+		try {
+			// Set server
+			$server = new Server(Config::get('NOTIFICATION_GOTIFY_URL'));
+		
+			// Set application token
+			$auth = new Token(Config::get('NOTIFICATION_GOTIFY_TOKEN'));
+		
+			// Create Message class instance
+			$message = new Message(
+				$server->get(),
+				$auth->get()
+			);
+		
+			// Send message
+			$message->create(
+				title: $this->config['title'],
+				message: $this->config['message'],
+				priority: $this->config['priority'],
+			);
+
+		} catch (EndpointException | GotifyException $err) {
+			throw new Exception('[Gotify error] ' . $err->getMessage());
+		}
+	}
+}
