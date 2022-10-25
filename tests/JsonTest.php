d295b51c3f9d8bc635ca7052972482d677d4d298
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-10-25 20:48:00 +0100

    Create JsonTest.php

---
+++ tests/JsonTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use Vigilant\Helper\Json;
+
+class JsonTest extends TestCase
+{
+    public function testEncodeValid(): void
+    {
+        self::assertEquals('{"foo":"bar"}', Json::encode(['foo' => 'bar']));
+    }
+
+    public function testDecodeValid(): void
+    {
+        $expected = new stdClass();
+        $expected->foo = 'bar';
+        self::assertEquals($expected, Json::decode('{"foo": "bar"}'));
+    }
+
+    public function testDecodeInvalid(): void
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('JSON Error: Syntax error');
+        Json::decode('foo');
+    }
+}
