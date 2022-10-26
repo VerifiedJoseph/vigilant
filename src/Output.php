64e1358c10870bb14587fc7376d736ef8835f6e9
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-10-26 14:35:29 +0100

    [Output] Add quiet() method for suppressing output

--- src/Output.php
+++ src/Output.php
@@ -5,12 +5,26 @@ namespace Vigilant;
 final class Output
 {
     /**
+     * @var bool $quiet Suppress output status
+     */
+    private static bool $quiet = false;
+
+    /**
+     * Suppress text output
+     */
+    public static function quiet() {
+        self::$quiet = true;
+    }
+
+    /**
      * Display text in terminal
      *
      * @param string $text Text string to display
      */
     public static function text(string $text = ''): void
     {
-        echo $text . "\n";
+        if (self::$quiet === false) {
+            echo $text . "\n";
+        }
     }
 }
