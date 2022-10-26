0ed824a484dcdc0c065418432320285bc1f6e91f
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-10-26 15:26:29 +0100

    [Feed\Feed] Add class

---
+++ src/Feed/Feed.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Vigilant\Feed;
+
+use Vigilant\Feed\Validate;
+use Vigilant\Feed\Details;
+
+final class Feed
+{
+    /**
+     * @var array $feed Feed entry from feeds.yaml
+     */
+    private array $feed = [];
+
+    /**
+     * Constructor
+     *
+     * @param array $feed
+     */
+    public function __construct(array $feed)
+    {
+        $this->feed = $feed;
+		$this->validate();
+    }
+
+	/**
+	 * Get details for feed entry
+	 * 
+	 * @return Details
+	 */
+    public function getDetails(): Details
+    {
+        return new Details($this->feed);
+    }
+
+	/**
+	 * Validate feed entry
+	 */
+	private function validate(): void
+	{
+		new Validate($this->feed);
+	}
+}
