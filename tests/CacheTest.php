d0954fc9d5a107d0e432ff49e4859328896b03e9
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-10-26 20:19:17 +0000

    Add Cache tests (#8)
    Adds tests for Cache class
    
    * Add fixture cache.json
    * Add fixture cache-default.json
    * Add Cache tests

---
+++ tests/CacheTest.php
@@ -0,0 +1,195 @@
+<?php
+
+use Vigilant\Cache;
+use Vigilant\Helper\Json;
+
+class CacheTest extends TestCase
+{
+    private static $tempCacheFolder = '';
+    private static $tempCacheFileName = '';
+
+    private static array $data = [];
+
+    private static Cache $cache;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$tempCacheFolder = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'vigilant-caching-tests';
+
+        mkdir(self::$tempCacheFolder);
+    }
+
+    public function setUp(): void
+    {
+        self::$tempCacheFileName = bin2hex(random_bytes(5));
+        $this->createTempCacheFile();
+
+        self::$data = Json::decode(self::loadFixture('cache.json'));
+
+        self::$cache = new Cache(
+            self::$tempCacheFolder,
+            self::$tempCacheFileName
+        );
+    }
+
+    /**
+     * Test getFeedUrl()
+     */
+    public function testGetFeedUrl(): void
+    {
+        $url = self::$cache->getFeedUrl();
+
+        $this->assertIsString($url);
+        $this->assertEquals(
+            self::$data['feed_url'],
+            $url
+        );
+    }
+
+    /**
+     * Test getFirstCheck()
+     */
+    public function testGetFirstCheck(): void
+    {
+        $check = self::$cache->getFirstCheck();
+
+        $this->assertIsInt($check);
+        $this->assertEquals(
+            self::$data['first_check'],
+            $check
+        );
+    }
+
+    /**
+     * Test getIsExpired()
+     */
+    public function testIsExpired(): void
+    {
+        $expired = self::$cache->isExpired();
+
+        $this->assertIsBool($expired);
+        $this->assertEquals(
+            true,
+            $expired
+        );
+    }
+
+    /**
+     * Test getNextCheck()
+     */
+    public function testGetNextCheck(): void
+    {
+        $check = self::$cache->getNextCheck();
+
+        $this->assertIsInt($check);
+        $this->assertEquals(
+            self::$data['next_check'],
+            $check
+        );
+    }
+
+    /**
+     * Test getItems()
+     */
+    public function testGetItems(): void
+    {
+        $items = self::$cache->getItems();
+
+        $this->assertIsArray($items);
+        $this->assertEquals(
+            self::$data['items'],
+            $items
+        );
+    }
+
+    /**
+     * test setFirstCheck
+     */
+    public function testSetFirstCheck(): void
+    {
+        $time = time();
+        $cache = new Cache(self::$tempCacheFolder, 'testing');
+
+        $this->assertEquals(0, $cache->getFirstCheck());
+
+        $cache->setFirstCheck();
+
+        $this->assertGreaterThanOrEqual($time, $cache->getFirstCheck());
+    }
+
+    /**
+     * test setFirstCheck()
+     */
+    public function testSetFeedUrl(): void
+    {
+        $url = 'https://www.example.org/feed.rss';
+        self::$cache->setFeedUrl($url);
+
+        $this->assertEquals(
+            $url,
+            self::$cache->getFeedUrl()
+        );
+    }
+
+    /**
+     * test updateNextCheck()
+     */
+    public function testUpdateNextCheck(): void
+    {
+        $time = time() + 300;
+        self::$cache->updateNextCheck(300);
+
+        $this->assertGreaterThanOrEqual(
+            $time,
+            self::$cache->getNextCheck()
+        );
+    }
+
+    /**
+     * Test UpdateItems()
+     */
+    public function testUpdateItems(): void
+    {
+        $items = ['iUz0s3QaHS', 'QwZF6yVJVu'];
+        self::$cache->updateItems($items);
+
+        $this->assertEquals(
+            $items,
+            self::$cache->getItems()
+        );
+    }
+
+    /**
+     * Test save()
+     */
+    public function testSave(): void
+    {
+        $cache = new Cache(self::$tempCacheFolder, 'testing');
+        $cache->save();
+
+        $this->assertFileExists(self::$tempCacheFolder . DIRECTORY_SEPARATOR . 'testing');
+        $this->assertJsonFileEqualsJsonFile(
+            self::$tempCacheFolder . DIRECTORY_SEPARATOR . 'testing',
+            $this->getFixturePath('cache-default.json')
+        );
+
+        unlink(self::$tempCacheFolder . DIRECTORY_SEPARATOR . 'testing');
+    }
+
+    public function tearDown(): void
+    {
+        unlink(self::$tempCacheFolder . DIRECTORY_SEPARATOR . self::$tempCacheFileName);
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        rmdir(self::$tempCacheFolder);
+    }
+
+    private function createTempCacheFile(): void
+    {
+        $file = self::$tempCacheFolder . DIRECTORY_SEPARATOR . self::$tempCacheFileName;
+
+        file_put_contents($file, self::loadFixture('cache.json'));
+    }
+}
