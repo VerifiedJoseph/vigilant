47ecb8795d6dcbfac806537f936101ab69b4757a
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-10-27 14:11:54 +0100

    Update FeedsException.php

--- src/Exception/FeedsException.php
+++ src/Exception/FeedsException.php
@@ -6,7 +6,7 @@ use Throwable;
 
 class FeedsException extends \Exception
 {
-    public function __construct($message, $code = 0, Throwable $previous = null)
+    public function __construct(string $message, int $code = 0, Throwable $previous = null)
     {
         $message = 'Feeds error: ' . $message;
 
