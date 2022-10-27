f2d2d17d8a33e0ffb9c355cb6a2e71f31713a096
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-10-27 01:21:08 +0100

    [CheckException] Update message prefix

--- src/Exception/CheckException.php
+++ src/Exception/CheckException.php
@@ -8,6 +8,8 @@ class CheckException extends \Exception
 {
     public function __construct($message, $code = 0, Throwable $previous = null)
     {
+        $message = '[Feed check error] ' . $message;
+
         parent::__construct($message, $code, $previous);
     }
 }
