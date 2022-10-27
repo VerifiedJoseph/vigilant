f1a9014707f441f0e15e4e216cc3a5ea8c6af481
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-10-27 02:57:27 +0100

    Add deamon.php

---
+++ deamon.php
@@ -0,0 +1,28 @@
+<?php
+
+use Vigilant\Config;
+use Vigilant\Feeds;
+use Vigilant\Output;
+
+use Vigilant\Exception\ConfigException;
+use Vigilant\Exception\AppException;
+
+require('vendor/autoload.php');
+
+try {
+    Config::load();
+
+    $feeds = new Feeds(
+        Config::getFeedsPath()
+    );
+
+    Output::text('Running...');
+
+    while (true) {
+        $feeds->check();
+
+        sleep(60);
+    }
+} catch (ConfigException | AppException $err) {
+    Output::text($err->getMessage());
+}
