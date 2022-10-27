bbe41112cfbe639152f701a7188ae16e726095c7
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-10-27 01:20:50 +0100

    [ConfigException] Update message prefix

--- src/Exception/ConfigException.php
+++ src/Exception/ConfigException.php
@@ -8,7 +8,7 @@ class ConfigException extends \Exception
 {
     public function __construct($message, $code = 0, Throwable $previous = null)
     {
-        $message = 'Config Error: ' . $message;
+        $message = '[Config error] ' . $message;
 
         parent::__construct($message, $code, $previous);
     }
