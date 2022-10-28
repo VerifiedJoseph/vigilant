bfe6a89386af35118bb03dbcd30707fcb7bc2158
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-10-28 23:05:12 +0100

    Use FeedIo

--- src/Exception/CheckException.php
+++ src/Exception/CheckException.php
@@ -6,10 +6,4 @@ use Throwable;
 
 class CheckException extends \Exception
 {
-    public function __construct(string $message, int $code = 0, Throwable $previous = null)
-    {
-        $message = '[Feed check error] ' . $message;
-
-        parent::__construct($message, $code, $previous);
-    }
 }
