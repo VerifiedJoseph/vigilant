46b02cea0ddb8f01a03f7be0a81792257e291798
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-03-05 14:16:44 +0000

    workflows: Add build action (#112)
    * Create build-dist.sh
    * Create build.yml

---
+++ build/build-dist.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Create dist folder
+mkdir ./dist
+
+# Copy folders
+cp -r ./docs ./dist/docs
+cp -r ./src ./dist/src
+cp -r ./vendor ./dist/vendor
+
+# Copy files
+cp ./composer.json ./dist/composer.json
+cp ./composer.lock ./dist/composer.lock
+cp ./config.example.php ./dist/pconfig.example.php
+cp ./feeds.example.yaml ./dist/feeds.example.yaml
+cp ./vigilant.php./dist/vigilant.php
+cp ./daemon.php ./dist/daemon.php
+cp ./README.md ./dist/README.md
+cp ./LICENSE ./dist/LICENSE
