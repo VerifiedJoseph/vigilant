e842d3fff1ad710b2cb6fbe1bf1eba59384732ab
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-03-26 19:19:46 +0000

    Improve test coverage for `Notify`

--- tests/NotifyTest.php
+++ tests/NotifyTest.php
@@ -33,6 +33,46 @@ class NotifyTest extends TestCase
     }
 
    /**
+    * Test creating Gotify instance with priority from feed details
+    */
+    public function testCreatingGotifyWithPriorityFromFeedDetails(): void
+    {
+        /** @var PHPUnit\Framework\MockObject\Stub&Config */
+        $config = self::createStub(Config::class);
+        $config->method('getNotificationService')->willReturn('gotify');
+        $config->method('getGotifyUrl')->willReturn('https://gotify.example.com/');
+        $config->method('getGotifyPriority')->willReturn(0);
+        $config->method('getGotifyToken')->willReturn('fake-token');
+
+        $feed = $this->feed;
+        $feed['gotify_priority'] = 5;
+
+        $notify = new notify(new Details($feed), $config);
+
+        $this->assertInstanceOf(Gotify::class, $notify->getClass());
+    }
+
+   /**
+    * Test creating Gotify instance with token from feed details
+    */
+    public function testCreatingGotifyWithTokenFromFeedDetails(): void
+    {
+        /** @var PHPUnit\Framework\MockObject\Stub&Config */
+        $config = self::createStub(Config::class);
+        $config->method('getNotificationService')->willReturn('gotify');
+        $config->method('getGotifyUrl')->willReturn('https://gotify.example.com/');
+        $config->method('getGotifyPriority')->willReturn(0);
+        $config->method('getGotifyToken')->willReturn('fake-token');
+
+        $feed = $this->feed;
+        $feed['gotify_token'] = 'qwerty';
+
+        $notify = new notify(new Details($feed), $config);
+
+        $this->assertInstanceOf(Gotify::class, $notify->getClass());
+    }
+
+   /**
     * Test creating Ntfy instance without auth
     */
     public function testCreatingNtfy(): void
@@ -42,7 +82,6 @@ class NotifyTest extends TestCase
         $config->method('getNotificationService')->willReturn('ntfy');
         $config->method('getNtfyUrl')->willReturn('https://ntfy.example.com/');
         $config->method('getNtfyPriority')->willReturn(0);
-        $config->method('getNtfyToken')->willReturn('fake-token');
 
         $notify = new notify(new Details($this->feed), $config);
 
@@ -59,7 +98,6 @@ class NotifyTest extends TestCase
         $config->method('getNotificationService')->willReturn('ntfy');
         $config->method('getNtfyUrl')->willReturn('https://ntfy.example.com/');
         $config->method('getNtfyPriority')->willReturn(0);
-        $config->method('getNtfyToken')->willReturn('fake-token');
         $config->method('getNtfyAuthMethod')->willReturn('password');
         $config->method('getNtfyUsername')->willReturn('bob');
         $config->method('getNtfyPassword')->willReturn('qwerty');
@@ -74,16 +112,72 @@ class NotifyTest extends TestCase
     */
     public function testCreatingNtfyWithTokenAuth(): void
     {
-         /** @var PHPUnit\Framework\MockObject\Stub&Config */
-         $config = self::createStub(Config::class);
-         $config->method('getNotificationService')->willReturn('ntfy');
-         $config->method('getNtfyUrl')->willReturn('https://ntfy.example.com/');
-         $config->method('getNtfyPriority')->willReturn(0);
-         $config->method('getNtfyToken')->willReturn('fake-token');
-         $config->method('getNtfyAuthMethod')->willReturn('token');
-         $config->method('getNtfyToken')->willReturn('bob');
+        /** @var PHPUnit\Framework\MockObject\Stub&Config */
+        $config = self::createStub(Config::class);
+        $config->method('getNotificationService')->willReturn('ntfy');
+        $config->method('getNtfyUrl')->willReturn('https://ntfy.example.com/');
+        $config->method('getNtfyPriority')->willReturn(0);
+        $config->method('getNtfyToken')->willReturn('fake-token');
+        $config->method('getNtfyAuthMethod')->willReturn('token');
 
         $notify = new notify(new Details($this->feed), $config);
+
+        $this->assertInstanceOf(Ntfy::class, $notify->getClass());
+    }
+
+   /**
+    * Test creating Ntfy instance with token from feed details
+    */
+    public function testCreatingNtfyWithTokenFromFeedDetails(): void
+    {
+        /** @var PHPUnit\Framework\MockObject\Stub&Config */
+        $config = self::createStub(Config::class);
+        $config->method('getNotificationService')->willReturn('ntfy');
+        $config->method('getNtfyUrl')->willReturn('https://ntfy.example.com/');
+        $config->method('getNtfyPriority')->willReturn(0);
+
+        $feed = $this->feed;
+        $feed['ntfy_token'] = 'qwerty';
+
+        $notify = new notify(new Details($feed), $config);
+
+        $this->assertInstanceOf(Ntfy::class, $notify->getClass());
+    }
+
+   /**
+    * Test creating Ntfy instance with topic from feed details
+    */
+    public function testCreatingNtfyWithTopicFromFeedDetails(): void
+    {
+        /** @var PHPUnit\Framework\MockObject\Stub&Config */
+        $config = self::createStub(Config::class);
+        $config->method('getNotificationService')->willReturn('ntfy');
+        $config->method('getNtfyUrl')->willReturn('https://ntfy.example.com/');
+        $config->method('getNtfyPriority')->willReturn(0);
+
+        $feed = $this->feed;
+        $feed['ntfy_topic'] = 'qwerty';
+
+        $notify = new notify(new Details($feed), $config);
+
+        $this->assertInstanceOf(Ntfy::class, $notify->getClass());
+    }
+
+   /**
+    * Test creating Ntfy instance with priority from feed details
+    */
+    public function testCreatingNtfyWithPriorityFromFeedDetails(): void
+    {
+        /** @var PHPUnit\Framework\MockObject\Stub&Config */
+        $config = self::createStub(Config::class);
+        $config->method('getNotificationService')->willReturn('ntfy');
+        $config->method('getNtfyUrl')->willReturn('https://ntfy.example.com/');
+        $config->method('getNtfyPriority')->willReturn(0);
+
+        $feed = $this->feed;
+        $feed['ntfy_priority'] = 5;
+
+        $notify = new notify(new Details($feed), $config);
 
         $this->assertInstanceOf(Ntfy::class, $notify->getClass());
     }
