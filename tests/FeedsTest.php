4441f201e2015276ab54e2b38ffff31722fe22bb
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-03-28 17:10:27 +0000

    Update FeedsTest.php

--- tests/FeedsTest.php
+++ tests/FeedsTest.php
@@ -7,16 +7,13 @@ use Vigilant\Exception\AppException;
 
 class FeedsTest extends TestCase
 {
-    public static function setUpBeforeClass(): void
-    {
-        Output::quiet();
-    }
-
     /**
      * Test get()
      */
     public function testGet(): void
     {
+        $this->expectOutputRegex('/Loading feeds.yaml/');
+
         /** @var PHPUnit\Framework\MockObject\Stub&Config */
         $config = $this->createStub(Config::class);
         $config->method('getFeedsPath')->willReturn(self::getSamplePath('feeds.yaml'));
@@ -38,12 +35,8 @@ class FeedsTest extends TestCase
 
         $this->expectException(AppException::class);
         $this->expectExceptionMessage('No feeds in feeds.yaml');
+        $this->expectOutputRegex('/Loading feeds.yaml/');
 
         new Feeds($config);
-    }
-
-    public static function tearDownAfterClass(): void
-    {
-        Output::disableQuiet();
     }
 }
