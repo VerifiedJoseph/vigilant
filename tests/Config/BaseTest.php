8e7b3d4aedfebd53bbcd89d7176054509470cbc4
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-04-03 15:07:55 +0100

    Update BaseTest.php

--- tests/Config/BaseTest.php
+++ tests/Config/BaseTest.php
@@ -1,11 +1,25 @@
 <?php
 
+use PHPUnit\Framework\Attributes\CoversClass;
 use Vigilant\Config\Base;
 
+#[CoversClass(Base::class)]
 class BaseTest extends TestCase
 {
     /**
-     * Test `getEnv` with no environment variable
+     * Test `getEnv()`
+     */
+    public function testGetEnv(): void
+    {
+        putenv('VIGILANT_TEST=hello');
+
+        $class = new class () extends Base {
+        };
+        $this->assertEquals('hello', $class->getEnv('TEST'));
+    }
+
+    /**
+     * Test `getEnv()` with no environment variable
      */
     public function testGetEnvEmptyValue(): void
     {
