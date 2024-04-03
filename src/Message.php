1a8bbc05d003c6e6f2f7885cb7e068c51550938f
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-04-03 19:46:26 +0100

    Create Message.php

---
+++ src/Message.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Vigilant;
+
+class message
+{
+    private string $title;
+    private string $body;
+    private string $url;
+
+    /**
+     * @param string $title Message title
+     * @param string $body Message body
+     * @param string $url Message URL
+     */
+    public function __construct(string $title, string $body, string $url = '')
+    {
+        $this->title = $title;
+        $this->body = $body;
+        $this->url = $url;
+    }
+
+    /**
+     * Returns message title
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * Returns message body
+     * @return string
+     */
+    public function getBody(): string
+    {
+        return $this->body;
+    }
+
+    /**
+     * Returns message URL
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+}
