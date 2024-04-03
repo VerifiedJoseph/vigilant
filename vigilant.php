26ebd0015ffd8b623ad75f8e6a17e464774b0dd2
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-04-03 17:07:27 +0100

    Update vigilant.php

--- vigilant.php
+++ vigilant.php
@@ -4,6 +4,7 @@ use Vigilant\Config;
 use Vigilant\Feeds;
 use Vigilant\Check;
 use Vigilant\Fetch;
+use Vigilant\Notify;
 use Vigilant\Output;
 use Vigilant\Exception\ConfigException;
 use Vigilant\Exception\AppException;
@@ -14,11 +15,18 @@ try {
     $config = new Config();
     $config->validate();
 
+    $fetch = new Fetch();
     $feeds = new Feeds($config);
     Output::newline();
 
-    foreach ($feeds->get() as $feed) {
-        $check = new Check($feed, $config, new Fetch());
+    foreach ($feeds->get() as $details) {
+        $notify = new Notify($details, $config);
+        $check = new Check(
+            $details,
+            $config,
+            $fetch,
+            $notify
+        );
         $check->run();
     }
 } catch (ConfigException | AppException $err) {
