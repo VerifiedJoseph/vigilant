5627d1e692889930cf526ad743d681a2bdbe5225
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-04-18 18:29:54 +0000

    Update daemon script (#152)

--- daemon.php
+++ daemon.php
@@ -2,6 +2,9 @@
 
 use Vigilant\Config;
 use Vigilant\Feeds;
+use Vigilant\Check;
+use Vigilant\Fetch;
+use Vigilant\Notify;
 use Vigilant\Output;
 use Vigilant\Exception\ConfigException;
 use Vigilant\Exception\AppException;
@@ -12,12 +15,31 @@ try {
     $config = new Config();
     $config->validate();
 
+    $fetch = new Fetch();
     $feeds = new Feeds($config);
 
     Output::text('Running...');
 
     while (true) {
-        $feeds->check();
+        foreach ($feeds->get() as $details) {
+            $notify = new Notify($details, $config);
+            $check = new Check(
+                $details,
+                $config,
+                $fetch
+            );
+
+            if ($check->isDue() === true) {
+                $check->check();
+                $notify->send($check->getMessages());
+
+                Output::text(sprintf(
+                    'Next check in %s seconds at %s',
+                    $details->getInterval(),
+                    $check->getNextCheckDate()
+                ));
+            }
+        }
 
         sleep(30);
     }
