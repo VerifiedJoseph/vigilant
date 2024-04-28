3b78c29f50318d6dcd42a5f86bb6165a704a832e
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-04-28 23:07:36 +0100

    Add logger class

---
+++ src/Logger.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Vigilant;
+
+use DateTime;
+use DateTimeZone;
+
+class Logger
+{
+    private DateTimeZone $timezone;
+
+    /**
+     * @param string $timezone Timezone
+     */
+    public function __construct(string $timezone)
+    {
+        $this->timezone = new DateTimeZone($timezone);
+    }
+
+    /**
+     * Display text in terminal
+     *
+     * @param string $text Text string to display
+     */
+    public function log(string $text): void
+    {
+        $date = new DateTime('now', $this->timezone);
+
+        echo sprintf(
+            '[Vigilant][%s] %s',
+            $date->format('Y-m-d h:s:i'),
+            $text
+        );
+    }
+
+    /**
+     * Output system newline character in terminal
+     */
+    public function newline(): void
+    {
+        echo PHP_EOL;
+    }
+}
