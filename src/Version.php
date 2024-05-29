f546831eb65806091588ea1fc07d95bfe1b28bfa
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-05-29 18:46:44 +0100

    Add cache format version to Version class

--- src/Version.php
+++ src/Version.php
@@ -10,10 +10,23 @@ class Version
     private static string $version = '1.2.0';
 
     /**
+     * @var string $cacheFormatVersion Cache format version
+     */
+    private static int $cacheFormatVersion = 1;
+
+    /**
      * Returns version number
      */
     public static function get(): string
     {
         return self::$version;
+    }
+
+    /**
+     * Returns cache format version
+     */
+    public static function getCacheFormatVersion(): int
+    {
+        return self::$cacheFormatVersion;
     }
 }
