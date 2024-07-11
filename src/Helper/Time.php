dcbf98515b6e20f246d2c8d621f31223f1ec2534
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-07-11 17:13:01 +0100

    Update Time.php

--- src/Helper/Time.php
+++ src/Helper/Time.php
@@ -11,12 +11,12 @@ class Time
      * @var array<int, string>
      */
     private static array $timeFormats = [
-        'g a',   // 1 am  - 12-hour without leading zeros and lowercase am/pm
-        'G a',   // 01 am - 12-hour with leading zeros and lowercase am/pm
-        'ga',    // 1am   - 12-hour without leading zeros, lowercase am/pm and no space
-        'Ga',    // 01am  - 12-hour with leading zeros, lowercase am/pm and no space
-        'H',     // 05    - 24-hour with leading zeros
-        'H:i'    // 15:10 - 24-hour with leading zeros and minutes
+        'g a', // 1 am  - 12-hour without leading zeros and lowercase am/pm
+        'G a', // 01 am - 12-hour with leading zeros and lowercase am/pm
+        'ga',  // 1am   - 12-hour without leading zeros, lowercase am/pm and no space
+        'Ga',  // 01am  - 12-hour with leading zeros, lowercase am/pm and no space
+        'H',   // 05    - 24-hour with leading zeros
+        'H:i'  // 15:10 - 24-hour with leading zeros and minutes
     ];
 
     /**
