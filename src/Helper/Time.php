8bfb610ce885c640fef8a6abed38fcc4812e60af
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-07-15 20:38:32 +0100

    Check if hours and minutes are valid in Time::isValid()

--- src/Helper/Time.php
+++ src/Helper/Time.php
@@ -11,28 +11,31 @@ class Time
      * @var array<int, string>
      */
     private static array $timeFormats = [
-        'H',   // 05    - 24-hour with leading zero
-        'G',   // 15    - 24-hour without leading zero
         'H:i', // 05:10 - 24-hour with leading zero and minutes
         'G:i', // 5:15  - 24-hour without leading zero and minutes
     ];
 
     /**
-     * Check if a time format is valid
+     * Check if a time is valid
      * @param string $time
      * @return bool
      */
     public static function isValid(string $time): bool
     {
-        $d = new DateTime();
-        $time = $d->format('Y-m-d ') . strtolower($time);
-
-        foreach (self::$timeFormats as $format) {
-            $date = DateTime::createFromFormat('Y-m-d ' . $format, $time);
-
-            if ($date !== false) {
-                return true;
+        try {
+            foreach (self::$timeFormats as $format) {
+                $date = DateTime::createFromFormat(
+                    'Y-m-d ' . $format,
+                    (new DateTime())->format('Y-m-d ') . strtolower($time)
+                );
+    
+                if ($date !== false) {
+                    new DateTime($time);
+                    return true;
+                }
             }
+        } catch (\Exception) {
+            return false;
         }
 
         return false;
--- tests/Helper/TimeTest.php
+++ tests/Helper/TimeTest.php
@@ -10,15 +10,12 @@ use Vigilant\Exception\AppException;
 #[UsesClass(AppException::class)]
 class TimeTest extends TestCase
 {
-    // 'H'   05    - 24-hour with leading zero
-    // 'G'   15    - 24-hour without leading zero
     // 'H:i' 05:10 - 24-hour with leading zero and minutes
     // 'G:i' 5:10  - 24-hour without leading zero and minutes
-    #[TestWith(['05', true])]
-    #[TestWith(['15', true])]
     #[TestWith(['05:10', true])]
     #[TestWith(['5:10', true])]
     #[TestWith(['1am', false])]
+    #[TestWith(['25:00', false])]
     public function testIsValid(string $time, bool $expected): void
     {
         $this->assertEquals($expected, Time::isValid($time));
