17db7e7a141e9701631440e49abe35cba0820beb
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-07-15 18:09:37 +0100

    Add ActiveHours class

---
+++ src/ActiveHours.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Vigilant;
+
+use DateTime;
+use DateTimeZone;
+
+class ActiveHours
+{
+    private Logger $logger;
+
+    private DateTime $now;
+    private DateTime $start;
+    private DateTime $end;
+
+    private DateTimeZone $timezone;
+    private bool $enabled = false;
+
+    private string $format = 'Y-m-d H:i:s e';
+
+    /**
+     * @param DateTime $now Current time and date
+     * @param ?string $startTime Start time
+     * @param ?string $endTime End time
+     * @param string $timezone Timezone
+     * @param Logger $logger
+     */
+    public function __construct(DateTime $now, ?string $startTime, ?string $endTime, string $timezone, Logger $logger)
+    {
+        $this->logger = $logger;
+
+        $this->now = $now;
+        $this->timezone = new DateTimeZone($timezone);
+
+        $this->process($startTime, $endTime);
+    }
+
+    /**
+     * Returns boolean indicating if time is in active hours window
+     * @return bool
+     */
+    public function isEnabled(): bool
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * Returns start time as a readable string
+     * @return string
+     */
+    public function getStartTime(): string
+    {
+        return $this->start->format($this->format);
+    }
+
+    /**
+     * Returns end time as a readable string
+     * @return string
+     */
+    public function getEndTime(): string
+    {
+        return $this->end->format($this->format);
+    }
+
+    /**
+     * @param ?string $startTime Start time
+     * @param ?string $endTime End time
+     */
+    private function process(?string $startTime, ?string $endTime): void
+    {
+        if ($startTime !== null && $endTime !== null) {
+            $this->start = new DateTime($startTime, $this->timezone);
+            $this->end = new DateTime($endTime, $this->timezone);
+            
+            if ($this->now >= $this->start && $this->now <= $this->end) {
+                $this->enabled = true;
+            }
+
+            $this->logger->debug('Active hours details:');
+            $this->logger->debug('Current time: ' . $this->now->format('Y-m-d H:i:s e'));
+            $this->logger->debug('Start time:   ' . $this->start->format('Y-m-d H:i:s e'));
+            $this->logger->debug('End time:     ' . $this->end->format('Y-m-d H:i:s e'));
+            $this->logger->debug('Enabled: ' . ($this->enabled ? 'true' : 'false'));
+        }
+    }
+}
---
+++ tests/ActiveHoursTest.php
@@ -0,0 +1,82 @@
+<?php
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
+use Vigilant\ActiveHours;
+use Vigilant\Logger;
+
+#[CoversClass(ActiveHours::class)]
+#[UsesClass(Logger::class)]
+class ActiveHoursTest extends TestCase
+{
+    private static Logger $logger;
+    private static string $timezone = 'UTC';
+    private string $formatRegex = '/[0-9]{4}+\-[0-9]{2}+\-[0-9]{2}+ [0-9]{2}+\:[0-9]{2}+\:[0-9]{2}+ UTC/';
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$logger = new Logger(self::$timezone);
+    }
+
+    /**
+     * Test class with time that is inside the active window
+     */
+    public function testActiveHoursInWindow(): void
+    {
+        $now = new DateTime('10:00', new DateTimeZone(self::$timezone));
+
+        $activeHours = new ActiveHours(
+            $now,
+            '06:00',
+            '20:00',
+            self::$timezone,
+            self::$logger
+        );
+
+        $this->assertTrue($activeHours->isEnabled());
+    }
+
+    /**
+     * Test class with time that is outside the active window
+     */
+    public function testActiveHoursOutsideWindow(): void
+    {
+        $now = new DateTime('05:00', new DateTimeZone(self::$timezone));
+
+        $activeHours = new ActiveHours(
+            $now,
+            '06:00',
+            '20:00',
+            self::$timezone,
+            self::$logger
+        );
+
+        $this->assertFalse($activeHours->isEnabled());
+    }
+
+    /**
+     * Test date and time formats returned by getStartTime() and getEndTime() with a regex
+     */
+    public function testGetMethodsDateAndTimeFormats()
+    {
+        $now = new DateTime('07:00', new DateTimeZone(self::$timezone));
+
+        $activeHours = new ActiveHours(
+            $now,
+            '06:00',
+            '20:00',
+            self::$timezone,
+            self::$logger
+        );
+
+        $this->assertMatchesRegularExpression(
+            $this->formatRegex,
+            $activeHours->getStartTime()
+        );
+
+        $this->assertMatchesRegularExpression(
+            $this->formatRegex,
+            $activeHours->getEndTime()
+        );
+    }
+}
