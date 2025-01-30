feedbbaade089b68b49b326e82a656c651356793
Joseph <VerifiedJoseph@users.noreply.github.com>
2025-01-30 14:51:32 +0000

    Add tests

--- tests/MessageTest.php
+++ tests/MessageTest.php
@@ -2,10 +2,18 @@
 
 use PHPUnit\Framework\Attributes\CoversClass;
 use Vigilant\Message;
+use Vigilant\Helper\Json;
 
 #[CoversClass(Message::class)]
 class MessageTest extends TestCase
 {
+    private static array $samples = [];
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$samples = Json::decode(self::loadSample('message-truncation.json'));
+    }
+
     public function testClass(): void
     {
         $title = 'Hello World';
@@ -34,5 +42,34 @@ class MessageTest extends TestCase
         $message = new Message($title, $body, $url, $prefix);
 
         $this->assertEquals($fullTitle, $message->getTitle());
+    }
+
+    /**
+     * Test message truncation with the default truncation length
+     */
+    public function testMessageTruncation(): void
+    {
+        $message = new Message(
+            title: '',
+            body: self::$samples['default']['input'],
+            truncate: true
+        );
+
+        $this->assertEquals(self::$samples['default']['output'], $message->getBody());
+    }
+
+    /**
+     * Test message truncation with a custom truncation length
+     */
+    public function testMessageTruncationCustomLength(): void
+    {
+        $message = new Message(
+            title: '',
+            body: self::$samples['custom']['input'],
+            truncate: true,
+            truncateLength: self::$samples['custom']['length']
+        );
+
+        $this->assertEquals(self::$samples['custom']['output'], $message->getBody());
     }
 }
