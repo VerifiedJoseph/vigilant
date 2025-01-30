0e1159176ce90a3004eb39a6c624dea9072f8997
Joseph <VerifiedJoseph@users.noreply.github.com>
2025-01-30 14:51:20 +0000

    Message: Add truncate method

--- src/Message.php
+++ src/Message.php
@@ -8,19 +8,28 @@ class Message
     private string $title;
     private string $body;
     private string $url;
+    private bool $truncate = false;
+    private int $truncateLength = 200;
 
     /**
      * @param string $title Message title
      * @param string $body Message body
      * @param string $url Message URL
      * @param ?string $prefix Message title prefix
+     * @param bool $truncate Truncate message status
+     * @param bool $truncateLength Number of characters to truncate message to.
      */
-    public function __construct(string $title, string $body, string $url = '', ?string $prefix = null)
+    public function __construct(string $title, string $body, string $url = '', ?string $prefix = null, bool $truncate = false, int $truncateLength = 200)
     {
         $this->title = $title;
         $this->body = $body;
         $this->url = $url;
         $this->prefix = $prefix;
+        $this->truncate = $truncate;
+
+        if ($truncateLength >= 0) {
+            $this->truncateLength = $truncateLength;
+        }
     }
 
     /**
@@ -46,6 +55,10 @@ class Message
      */
     public function getBody(): string
     {
+        if ($this->truncate === true) {
+            return $this->truncate($this->body);
+        }
+
         return $this->body;
     }
 
@@ -56,5 +69,26 @@ class Message
     public function getUrl(): string
     {
         return $this->url;
+    }
+
+    /**
+     * Truncate message body
+     * @param string $text
+     * @return string
+     */
+    private function truncate(string $text): string
+    {
+        if(strlen($text) <= $this->truncateLength) {
+            return $text;
+        }
+
+        $text = substr($text, 0, $this->truncateLength);
+        $breakpoint = strrpos($text, '.');
+
+        if($breakpoint !== false) {
+            $text = substr($text, 0, $breakpoint);
+        }
+
+        return $text . '...';
     }
 }
