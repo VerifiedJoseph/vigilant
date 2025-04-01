4089ddccf0d69b96302f7966fa8213706510b9bc
Joseph <VerifiedJoseph@users.noreply.github.com>
2025-04-01 15:55:40 +0100

    Version 1.5.1 (#385)

--- src/Version.php
+++ src/Version.php
@@ -9,7 +9,7 @@ class Version
     /**
      * @var string $version Vigilant version
      */
-    private static string $version = '1.5.0';
+    private static string $version = '1.5.1';
 
     /**
      * @var int $cacheFormatVersion Cache format version
