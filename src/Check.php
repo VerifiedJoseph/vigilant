d0354d7eccabd33641f26cf19391f4f1c0e84c0b
Philip Rosenberg-Watt <PhilRW@users.noreply.github.com>
2025-06-09 11:17:01 -0600

    Check: Fallback to getSummary() (#404)
    This will fix ATOM feeds that don't have `<content>` but rather a
    `<summary>` instead.
    
    Also closes #403

--- src/Check.php
+++ src/Check.php
@@ -142,7 +142,7 @@ final class Check
                 $newItems += 1;
 
                 $title = html_entity_decode($item->getTitle());
-                $body = strip_tags(html_entity_decode($item->getContent()));
+                $body = strip_tags(html_entity_decode($item->getContent() ?? $item->getSummary() ?? ''));
 
                 $this->logger->info(sprintf('Found...%s (%s)', $title, $hash));
 
