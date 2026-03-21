6f31bee3ca550efbbf0c1ed5333aa6c5da0fb789
Joseph <VerifiedJoseph@users.noreply.github.com>
2026-03-21 13:06:53 +0000

    UserAgent: Add class

---
+++ src/UserAgent.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Vigilant;
+
+class UserAgent
+{
+	/**
+     * Returns default user-agent string `Vigilant/VERSION (https://github.com/VerifiedJoseph/vigilant)`
+     * @return string
+     */
+    public static function getDefault(): string
+    {
+        return sprintf('Vigilant/%s (https://github.com/VerifiedJoseph/vigilant)', Version::get());
+    }
+}
