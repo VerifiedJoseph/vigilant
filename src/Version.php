a6d1f58bb784fdb8ddf92e9e7f5d2cecbf37c1c8
Joseph <VerifiedJoseph@users.noreply.github.com>
2026-08-03 18:30:47 +0100

    Bump version

--- src/Version.php
+++ src/Version.php
@@ -9,7 +9,7 @@ class Version
     /**
      * @var string $version Vigilant version
      */
-    private static string $version = '1.9.2';
+    private static string $version = '1.10.0';
 
     /**
      * @var int $cacheFormatVersion Cache format version
